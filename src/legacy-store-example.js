46620c17c65fd5f70e405a411953564e01165cd5
Michael <mikek222@gmail.com>
2026-01-16 12:53:32 -0500

    added first step of toolkit

---
+++ src/legacy-store-example.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { thunk } from "redux-thunk";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+
+
+export default store
--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
 
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
-
-
-export default store
+export default store;
